ffb1fd25945b19ef7154f67c660effb1d289a928
Codememory <kostynd1@gmail.com>
2023-09-29 12:19:25 +0300

    Fix bug

--- src/Decorator/ERC/CallbackWithEntityRepository.php
+++ src/Decorator/ERC/CallbackWithEntityRepository.php
@@ -2,8 +2,10 @@
 
 namespace Codememory\ApiBundle\Decorator\ERC;
 
+use Attribute;
 use Codememory\EntityResponseControl\Interfaces\DecoratorInterface;
 
+#[Attribute(Attribute::TARGET_PROPERTY)]
 final readonly class CallbackWithEntityRepository implements DecoratorInterface
 {
     public function __construct(
